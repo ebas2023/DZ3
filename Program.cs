cc26ef81aa17f2c4c0b51dc17c69bba5f71710d2
Vasya Pupkin <vasya.pupkin@email.com>
2023-07-31 21:35:06 +0300

    2 задача

--- Program.cs
+++ Program.cs
@@ -1,24 +1,21 @@
-﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом(первое == последнему, второе == предпоследнему).
-14212 -> нет
-12821 -> да
-23432 -> да*/
+﻿/*Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
 
-Console.WriteLine("Введите 5-е число");
-int num = int.Parse(Console.ReadLine()!);
-string five = num.ToString();
-if (five.Length == 5)
-{
-    if (five[0] == five[4] && five[1] == five[3])
-            {
-        Console.WriteLine("Число полнидром");
-    }
-    else
-    {
-        Console.WriteLine("Число не полнидром");
-    }
-  
-}
-else
-{
-    Console.WriteLine("Введите корректное число");
-}
+Console.Write("Введите координату точки А, x: ");
+int x1=int.Parse(Console.ReadLine ()!);
+Console.Write("Введите координату точки А, y: ");
+int y1=int.Parse(Console.ReadLine ()!);
+Console.Write("Введите координату точки А, z: ");
+int z1=int.Parse(Console.ReadLine ()!);
+
+Console.Write("Введите координату точки B, x: ");
+int x2=int.Parse(Console.ReadLine ()!);
+Console.Write("Введите координату точки B, y: ");
+int y2=int.Parse(Console.ReadLine ()!);
+Console.Write("Введите координату точки B, z: ");
+int z2=int.Parse(Console.ReadLine ()!);
+
+double AB=Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
+Console .WriteLine ($"Расстояние между точками {AB:f2}");
